f0847ef127b116df2c96fd17362c7a700168627d
Nicholas Land <nland22@gmail.com>
2013-05-16 11:00:23 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blackjack::Application.config.secret_token = '076bfe86985133ef921007cd8db4b92c9cf49c24d181314f6f6478328ddd4ae744ea6c730e8ea9b66d668262be0dbb5b1fb1e0c57fe9a80e10d53b0ad2217936'
