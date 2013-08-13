ec2b63c0bd3fcba89530b12e62d35675e9e092c1
Nicholas Land <nland22@gmail.com>
2013-08-13 17:06:38 -0400

    Minor file fixes. Nothing really changed in the application.

--- spec/javascripts/user_spec.js
+++ spec/javascripts/user_spec.js
@@ -1,17 +1,17 @@
 #= require application
 
 // describe('User', function(){
-// 
+//
 //   var server;
 //   beforeEach( function() {
 //     this.server = sinon.fakeServer.create();
 //   });
-// 
+//
 //   afterEach( function() {
 //     this.server.restore();
 //   });
-// 
-// 
+//
+//
 //   it ('should have a user', function() {
 //     this.server.respondWith("GET", "/api/user/",
 //                        [200, { "Content-Tyoe": "application/json"},
